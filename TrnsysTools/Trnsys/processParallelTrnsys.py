19792591486949ec0f3610bbeaf07969d70d5112
Mattia Battaglia <mattia.battaglia@spf.ch>
2019-05-22 13:56:48 +0200

    Now usable as a normal Python package
    Change name of source code folder and add __init__.py files as well as import commands to have a proper python package that does not conflict with other packages.

---
+++ TrnsysTools/Trnsys/processParallelTrnsys.py
@@ -0,0 +1,371 @@
+#!/usr/bin/python
+"""
+Main class to process all TRNSYS results.
+We need to include in this class any processing Class
+customized for new projects
+Author : Daniel Carbonell
+Date   : 01-10-2018
+ToDo : Copy config file to results folder automatically, remove processDataGshp and make it generic
+       getBaseClass should be defined outside this function so that this class is not changet at all
+"""
+
+import os
+import TrnsysTools.processingSimulations.debugProcess
+import multiprocessing as mp
+import runParallel as run
+import bigIceTrnsysClass as ice
+import TrnsysTools.utilities.utilsSpf as utils
+import readConfigTrnsys as readConfig
+import GshpTrnsysBaseClass as gshp
+import AlStoreTrnsysBaseClass as alu
+import p2GErlackerTrnsysClass as erlacker
+import warnings
+#we would need to pass the Class as inputs
+
+
+def processDataGeneral(casesInputs):
+
+    (baseClass,locationPath, fileName, avoidUser, maxMinAvoided, yearReadedInMonthlyFile, cleanModeLatex, firstMonthUsed,\
+      processQvsT,firstMonthUsed,buildingArea,dllTrnsysPath,setPrintDataForGle,firstConsideredTime) = casesInputs
+
+    print "starting processing of: " + fileName
+    #    locationPath = inputs.pop(0)
+    #    fileName,avoidUser,maxMinAvoided,yearReadedInMonthlyFile,cleanModeLatex,firstMonthUsed,processQvsT
+
+    test = baseClass
+
+    test.setBuildingArea(buildingArea)
+    test.setTrnsysDllPath(dllTrnsysPath)
+
+    # test.setTrnsysVersion("TRNSYS17_EXE")
+
+    test.setPrintDataForGle(setPrintDataForGle)
+
+    test.avoidUserDefinedCalculation = avoidUser
+    test.maxMinAvoided = maxMinAvoided
+    test.yearReadedInMonthylFile = yearReadedInMonthlyFile
+    test.cleanModeLatex = cleanModeLatex
+    test.firstConsideredTime = firstConsideredTime
+
+    myFirstMonthLong = utils.getMonthLongName(firstMonthUsed + 1)  # starts at 1
+    test.firstMonth = myFirstMonthLong
+    test.firstMonthIndex = 0  # firstMonthUsed
+
+    splitMonths = False
+
+    if (splitMonths == True):
+        # monthsSplit = [1,2,3,4,5,6,7,8,9,10,11,12]  #[5,10,11,12]
+        monthsSplit = [3, 4, 5, 6]  # [5,10,11,12]
+
+    else:
+        monthsSplit = []
+
+    if (processQvsT == True):
+        test.loadQvsT("QVsT.Plt", monthsSplit=monthsSplit, addDhwCirc=False, normalized=True)
+
+    doProcess = True
+
+    if (doProcess):
+        test.loadAndProcess()
+
+    # rename files if multiple years are available:
+    if yearReadedInMonthlyFile != -1:
+        renameFile = os.path.join(locationPath, fileName, fileName)
+
+        fileEndingsDefault = ["-results.dat", "-report.pdf"]
+
+        for ending in fileEndingsDefault:
+            newEnding = "-Year%i" % yearReadedInMonthlyFile + ending
+            try:
+                os.rename(renameFile + ending, renameFile + newEnding)
+            except:
+                warnings.warn(
+                    "File %s already exists, and thus was not saved again, needs to be improved (either not processed, or actually replaced)" % (
+                                renameFile + newEnding))
+
+        # rename files if multiple years are available:
+    if yearReadedInMonthlyFile != -1:
+        renameFile = os.path.join(locationPath, fileName, fileName)
+
+        fileEndingsDefault = ["-results.dat", "-report.pdf"]
+
+        for ending in fileEndingsDefault:
+            newEnding = "-Year%i" % yearReadedInMonthlyFile + ending
+            try:
+                os.rename(renameFile + ending, renameFile + newEnding)
+            except:
+                warnings.warn(
+                    "File %s already exists, and thus was not saved again, needs to be improved (either not processed, or actually replaced)" % (
+                                renameFile + newEnding))
+
+    del test  # time.sleep(5)
+
+    return " Finished: " + fileName
+
+
+def processDataGshp(casesInputs):
+#Include this in the generic function
+
+    (locationPath, fileName, avoidUser, maxMinAvoided, yearReadedInMonthlyFile, cleanModeLatex, firstMonthUsed,\
+      processQvsT,firstMonthUsed,buildingArea,dllTrnsysPath,setPrintDataForGle,firstConsideredTime) = casesInputs
+
+    print "starting processing of: " + fileName
+
+    test = gshp.GshpTrnsysBaseClass(locationPath, fileName)
+
+    test.setBuildingArea(buildingArea)
+    test.setTrnsysDllPath(dllTrnsysPath)
+
+    test.setPrintDataForGle(setPrintDataForGle)
+
+    test.avoidUserDefinedCalculation = avoidUser
+    test.maxMinAvoided = maxMinAvoided
+    test.yearReadedInMonthylFile = yearReadedInMonthlyFile
+    test.cleanModeLatex = cleanModeLatex
+    test.firstConsideredTime = firstConsideredTime
+
+    myFirstMonthLong = utils.getMonthLongName(firstMonthUsed + 1)  # starts at 1
+    test.firstMonth = myFirstMonthLong
+    test.firstMonthIndex = 0  # firstMonthUsed
+
+    splitMonths = False
+
+    if (splitMonths == True):
+        monthsSplit = [3, 4, 5, 6]  # [5,10,11,12]
+    else:
+        monthsSplit = []
+
+    if (processQvsT == True):
+        test.loadQvsT("QVsT.Plt", monthsSplit=monthsSplit, addDhwCirc=True, normalized=True)
+
+    doProcess = True
+
+    if (doProcess):
+        test.loadAndProcess()
+
+    del test
+
+    return " Finished: " + fileName
+
+
+
+class ProcessParallelTrnsys():
+
+    def __init__(self):
+
+        self.defaultInputs()
+
+        self.filteredfolder = [".gle"]
+
+    def defaultInputs(self):
+
+        self.inputs = {}
+        self.inputs["processParallel"] = True
+        self.inputs["avoidUser"]    = False
+        self.inputs["processQvsT"]  = True
+        self.inputs["cleanModeLatex"] = False
+        self.inputs["maxMinAvoided"] = False
+        self.inputs["yearReadedInMonthlyFile"] = -1
+        self.inputs["process"] = True
+        self.inputs["firstMonthUsed"] = 6     # 0=January 1=February 7=August
+        self.inputs["reduceCpu"] = 0
+        self.inputs["typeOfProcess"] = "completeFolder" # "casesDefined"
+        self.inputs["forceProcess"]  =  True #even if results file exist it proceess the results, otherwise it checks if it exists
+        self.inputs["pathBase"] = False
+        self.inputs["setPrintDataForGle"] = True
+        self.inputs['firstConsideredTime'] = None #Be carefull here. Thsi will not be proprly filtered
+        self.inputs["buildingArea"] = 1072.
+        self.inputs["parseFileCreated"] = False
+        self.inputs["processQvsT"] = True
+        self.inputs["dllTrnsysPath"] = False
+        self.inputs["classProcessing"] = False
+
+    def setFilteredFolders(self,foldersNotUsed):
+        self.filteredfolder = foldersNotUsed
+
+    def readConfig(self,path,name,parseFileCreated=False):
+
+        tool = readConfig.ReadConfigTrnsys()
+        tool.readFile(path,name,self.inputs,parseFileCreated=parseFileCreated)
+
+    def getBaseClass(self,classProcessing,pathFolder,fileName):
+
+        raise ValueError("This function needs to be defined for each processing case")
+
+        if (classProcessing == "Erlacker"):
+            baseClass = erlacker.P2GErlackerTrnsysClass(pathFolder,fileName)
+        elif (classProcessing == "AlStore"):
+            baseClass = alu.AlStoreTrnsysBaseClass(pathFolder,fileName)
+        elif (classProcessing == "BigIce"):
+            baseClass = ice.BigIceTrnsysClass(pathFolder,fileName)
+        else:
+            baseClass = None
+
+        return baseClass
+
+    def process(self):
+
+        casesInputs = []
+        fileName = []
+        classList = []
+
+        if (self.inputs["typeOfProcess"] == "completeFolder"):
+
+            pathFolder = self.inputs["pathBase"]
+            fileName = [name for name in os.listdir(pathFolder) if os.path.isdir(pathFolder + "\\" + name)]
+
+            for name in fileName:
+
+                folderUsed = True
+                for i in range(len(self.filteredfolder)):
+                    if (name == self.filteredfolder[i]):
+                        folderUsed=False
+                if(folderUsed):
+                    nameWithPath = os.path.join(pathFolder, "%s\\%s-results.dat" % (name, name))
+
+                    if (os.path.isfile(nameWithPath) and self.inputs["forceProcess"] == False):
+                        print "file :%s already processed" % name
+
+                    elif os.path.isfile(os.path.join(pathFolder, "%s\\%s-Year1-results.dat" % (name, name))) and  self.inputs["forceProcess"] == False:
+                        print "file :%s already processed" % name
+
+                    else:
+                        baseClass = self.getBaseClass(self.inputs["classProcessing"],pathFolder,name)
+
+
+
+                        print "file :%s will be processed" % name
+                        casesInputs.append((baseClass,pathFolder, name, self.inputs["avoidUser"],self.inputs["maxMinAvoided"],self.inputs["yearReadedInMonthlyFile"],\
+                                            self.inputs["cleanModeLatex"],self.inputs["firstMonthUsed"],self.inputs["processQvsT"],self.inputs["firstMonthUsed"],self.inputs["buildingArea"],\
+                                            self.inputs["dllTrnsysPath"],self.inputs["setPrintDataForGle"],self.inputs["firstConsideredTime"]))
+
+        elif self.inputs["typeOfProcess"] == "casesDefined":
+
+            #for city in self.inputs["cities"]:
+            #    for fileType in self.inputs["fileTypes"]:
+
+            #        name = self.inputs["fileName"]+"-%s_%s"%(city,fileType)
+            #        pathFolder = os.path.join(self.inputs["pathBase"],city)
+            name = self.inputs["fileName"]
+            pathFolder = self.inputs["pathBase"]
+
+            fileName.append(name)
+
+            folderUsed = True
+            for i in range(len(self.filteredfolder)):
+                if (name == self.filteredfolder[i]):
+                    folderUsed = False
+            if (folderUsed):
+                nameWithPath = os.path.join(pathFolder, "%s\\%s-results.dat" % (name, name))
+
+                if (os.path.isfile(nameWithPath) and self.inputs["forceProcess"] == False):
+                    print "file :%s already processed" % name
+                else:
+                    print "file :%s will be processed" % name
+                    
+
+                    baseClass = self.getBaseClass(self.inputs["classProcessing"], pathFolder,self.inputs["fileName"])
+
+
+                    casesInputs.append((baseClass,pathFolder, name, self.inputs["avoidUser"], self.inputs["maxMinAvoided"],
+                                    self.inputs["yearReadedInMonthlyFile"], \
+                                    self.inputs["cleanModeLatex"], self.inputs["firstMonthUsed"],
+                                    self.inputs["processQvsT"], self.inputs["firstMonthUsed"],
+                                    self.inputs["buildingArea"], \
+                                    self.inputs["dllTrnsysPath"], self.inputs["setPrintDataForGle"],
+                                    self.inputs["firstConsideredTime"]))
+
+        elif self.inputs["typeOfProcess"] == "citiesFolder":
+
+            for city in self.inputs["cities"]:
+                pathFolder = os.path.join(self.inputs["pathBase"],city)
+                fileName = [name for name in os.listdir(pathFolder) if os.path.isdir(pathFolder + "\\" + name)]
+
+                for name in fileName:
+
+                    folderUsed = True
+                    for i in range(len(self.filteredfolder)):
+                        if (name == self.filteredfolder[i]):
+                            folderUsed = False
+                    if (folderUsed):
+                        nameWithPath = os.path.join(pathFolder, "%s\\%s-results.dat" % (name, name))
+
+                        if (os.path.isfile(nameWithPath) and self.inputs["forceProcess"] == False):
+                            print "file :%s already processed" % name
+
+                        elif os.path.isfile(os.path.join(pathFolder, "%s\\%s-Year1-results.dat" % (name, name))) and self.inputs["forceProcess"] == False:
+                            print "file :%s already processed" % name
+
+                        else:
+                            baseClass = self.getBaseClass(self.inputs["classProcessing"], pathFolder, name)
+
+                            print "file :%s will be processed" % name
+
+
+                            if "hourly" in name and not "Mean" in name:
+                                for i in range(self.inputs["numberOfYearsInHourlyFile"]):
+                                    casesInputs.append((baseClass, pathFolder, name, self.inputs["avoidUser"], self.inputs["maxMinAvoided"],
+                                                i+1, self.inputs["cleanModeLatex"], self.inputs["firstMonthUsed"],
+                                                self.inputs["processQvsT"], self.inputs["firstMonthUsed"],
+                                                self.inputs["buildingArea"], \
+                                                self.inputs["dllTrnsysPath"], self.inputs["setPrintDataForGle"],
+                                                self.inputs["firstConsideredTime"]))
+                            else:
+                                casesInputs.append((baseClass, pathFolder, name, self.inputs["avoidUser"],
+                                                    self.inputs["maxMinAvoided"],
+                                                    self.inputs["yearReadedInMonthlyFile"], \
+                                                    self.inputs["cleanModeLatex"], self.inputs["firstMonthUsed"],
+                                                    self.inputs["processQvsT"], self.inputs["firstMonthUsed"],
+                                                    self.inputs["buildingArea"], \
+                                                    self.inputs["dllTrnsysPath"], self.inputs["setPrintDataForGle"],
+                                                    self.inputs["firstConsideredTime"]))
+
+            #sort to process 10 year files first and all 10 years:
+
+        else:
+            raise ValueError("Not Implemented yet")
+
+        if(self.inputs["processParallel"]==True):
+
+            debug = debugProcess.DebugProcess(pathFolder, "FileProcessed.dat", fileName)
+            debug.start()
+
+            # maximum number of processes at once:
+            maxNumberOfCPU = min(run.getNumberOfCPU() - self.inputs["reduceCpu"], len(fileName))
+
+            pool = mp.Pool(processes=maxNumberOfCPU)
+
+            results = pool.map(processDataGeneral,casesInputs)
+
+            # if(self.inputs["classProcessing"]=="BigIce"):
+            #     results = pool.map(processDataGeneral,casesInputs)
+            if(self.inputs["classProcessing"]=="GSHP"):
+                results = pool.map(processDataGshp,casesInputs)
+            else:
+                results = pool.map(processDataGeneral, casesInputs)
+
+            pool.close()
+
+            debug.addLines(results)
+            debug.finish()
+        else:
+            for i in range(len(casesInputs)):
+                if (self.inputs["classProcessing"] == "GSHP"):
+                    processDataGshp(casesInputs[i])
+                else:
+                    # test = erlacker.P2GErlackerTrnsysClass(pathFolder,fileName[i])
+                    processDataGeneral(casesInputs[i])
+
+    def changeFile(self,source,end):
+
+        # todo: this function is currently not working
+
+        found=False
+        for i in range(len(self.lines)):
+            # self.lines[i].replace(source,end)
+            if(self.lines[i]==source):
+                self.lines[i] = end
+                found=True
+
+        if(found==False):
+            print Warning("changeFile was not able to change %s by %s"%(source,end))
---
+++ TrnsysTools/processingSimulations/checkItProblemsClass.py
@@ -0,0 +1,132 @@
+
+#!/usr/bin/env python
+
+"""
+Class for analysis of simulation data to facilitate debugging
+
+Author : Dani Carbonell
+Date : xx.xx.2018
+ToDo :
+"""
+
+
+import os
+import processTrnsysFiles as processTrnsys
+import TrnsysTools.processingData.loadBaseNumpy as load
+import TrnsysTools.Trnsys.LogTrnsys as LogTrnsys
+import TrnsysTools.Trnsys.deckTrnsys as deckTrnsys
+
+
+class checkIterationProblemsClass(processTrnsys.ProcessTrnsys):
+
+    def __init__(self, _path, _name):
+
+        processTrnsys.ProcessTrnsys.__init__(self, _path, _name)
+
+    def loadStatus(self,_name):
+
+        useOnlyOneYear = False
+        firstConsideredTime = None
+        year = False
+
+        if (year == False):
+            self.readTrnsysFiles.readUserDefinedFiles(_name, firstConsideredTime=firstConsideredTime,
+                                                      useOnlyOneYear=useOnlyOneYear)
+        else:
+            self.readTrnsysFiles.readUserDefinedFilesYear(_name, year)
+
+        if (firstConsideredTime == None):
+            self.firstConsideredTime = self.readTrnsysFiles.initialTime
+        else:
+            self.firstConsideredTime = firstConsideredTime
+
+        self.nameVariables = self.readTrnsysFiles.name
+        self.variables     = self.readTrnsysFiles.load.variables
+        self.nData         = self.readTrnsysFiles.load.numberOfDataPoints
+
+        if(self.nameVariables[0]!="TIME"):
+            raise ValueError("File %s must have TIME as first variable"%_name)
+        else:
+            self.error = (self.variables[0][2]-self.variables[0][1])*0.1
+#This is for cases where the file has been generated with the standard process
+    def readItProblemsFile(self,name):
+
+        nameWithPath=os.path.join(self.outputPath,name)
+
+        itFile = load.loadBaseNumpy(nameWithPath)
+        itFile.loadFile(skypChar="!",splitArgument="\t")
+        self.hourWhereItProblems = itFile.getByIndex(1)
+#Here we read the log file as done in the process class without having to print the file and to actually process
+#the whole case only for the it Problems
+    def readItProblemsFromLog(self):
+
+        log = LogTrnsys.LogTrnsys(self.outputPath,self.fileName)
+        log.loadLog()
+        log.getMyDataFromLog()
+        log.getIteProblemsForEachMonth()
+
+        self.hourWhereItProblems = log.hourWhereItProblems
+        self.unitsItProblems = log.unitsInvolvedItProlems
+
+        deck = deckTrnsys.DeckTrnsys(self.outputPath,self.fileName)
+        deck.loadDeckWithoutComments()
+        deck.readAllTypes()
+
+        self.namesUnitsItProblems = []
+        for i in range(len(self.unitsItProblems)):
+            listNames = []
+            for j in range(len(self.unitsItProblems[i])):
+                unit = int(self.unitsItProblems[i][j])
+                for k in range(len(deck.TrnsysUnits)):
+                    if(deck.TrnsysUnits[k]==unit):
+                        name = deck.getTypeName(deck.TrnsysTypes[k])
+                        listNames.append(name)
+            self.namesUnitsItProblems.append(listNames)
+
+    def writeItFileWithSatus(self,name,limit=100):
+
+        lines=""
+        line="Number of iteration problems %f\n "%len(self.hourWhereItProblems);lines=lines+line
+
+        if(limit==None):
+            limit = len(self.hourWhereItProblems)
+
+        count=0
+        for t in range(len(self.hourWhereItProblems)):
+            if(count>limit):
+                pass
+            else:
+                time = self.hourWhereItProblems[t]
+                for i in range(self.nData):
+                    #I assume fist variable is TIME
+
+                    if(abs(self.variables[0][i]-time)<self.error): #error is the time step /10.
+                        count = count + 1
+                        line = "========================================\n";lines=lines+line
+                        line = "Iteration problem at :%f\nInvolved Types :" % time;lines=lines+line
+                        for m in range(len(self.namesUnitsItProblems[t])):
+                            line = "%s:%s\t"%(self.unitsItProblems[t][m],self.namesUnitsItProblems[t][m]);lines=lines+line
+                        line="\n";lines=lines+line
+                        line = "========================================\n";lines=lines+line
+
+                        for j in range(i-1,i+1,1):
+                            for k in range(len(self.variables)):
+                                line ="%s=%8.3f\t"%(self.nameVariables[k],self.variables[k][j]);lines=lines+line
+                            line="\n";lines=lines+line
+
+
+        nameWithPath=os.path.join(self.outputPath,name)
+
+        print "file Printed :%s"%nameWithPath
+
+        outfile = open(nameWithPath, 'w')
+        outfile.writelines(lines)
+        outfile.close()
+
+        print "End of printing errors in file:%s"%nameWithPath
+
+
+
+
+
+
