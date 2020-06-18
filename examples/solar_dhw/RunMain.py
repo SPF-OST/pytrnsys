1075436bace35f6a88dd049899ffb529b03ea203
Mattia Battaglia <mattia.battaglia@spf.ch>
2020-06-18 18:25:21 +0200

    new examples

--- examples/solar_dhw/RunMain.py
+++ examples/solar_dhw/RunMain.py
@@ -1,5 +1,6 @@
 import pytrnsys.rsim.runParallelTrnsys as runTrnsys
+import os
 
-pathConfig  = "pathToTheConfigFile"
-configFile = "DualRun.config"
+pathConfig  = "C:\Daten\OngoingProject\pytrnsysTest2\\testScaling"
+configFile = "run_solar_dhw.config"
 runTool = runTrnsys.RunParallelTrnsys(pathConfig,configFile=configFile)
