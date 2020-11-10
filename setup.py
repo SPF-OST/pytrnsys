d9eb8d30faef728fc621fe656599755dde6f48fa
martin-neugebauer <martin.neugebauer@spf.ch>
2020-11-10 11:21:21 +0100

    forgot to change download link before

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     #     "Documentation": "https://pytrnsys.readthedocs.io",
     #     "Source Code": "https://github.com/dcarbonellsanchez/pytrnsys",
     # },
-    download_url = "https://github.com/dcarbonellsanchez/pytrnsys/archive/0.3.tar.gz",
+    download_url = "https://github.com/dcarbonellsanchez/pytrnsys/archive/0.4.tar.gz",
     include_package_data=True,
     install_requires=[
         'numpy',
