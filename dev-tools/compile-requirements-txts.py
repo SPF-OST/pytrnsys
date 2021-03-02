d1f50659cb658750efe73b26fdec17266c1876a7
Damian Birchler <damian.birchler@ost.ch>
2021-03-02 12:01:32 +0100

    Add requirements.in's for different use cases.
    - Plus script to compile them to requirements.txt.

---
+++ dev-tools/compile-requirements-txts.py
@@ -0,0 +1,47 @@
+"""This script compiles the requirements.txt files corresponding to all known requirements.in files.
+
+If you want to add a dependency to pytrnsys add it to the relevant requirement.in file(s). Then invoke
+this script from within your virtual environment from the top-level pytrnsys directory like so:
+
+    python dev-tools\compile-requirements-txts.py -P <your-package>
+
+E.g., to add the package `pdfdiff` which is only needed for testing you'd add it to the
+requirements\test\requirements.in file and then run
+
+        python dev-tools\compile-requirements-txts.py -P pdfdiff
+
+Finally, sanity-check the newly generated requirements.txt file(s) by diffing them against the checked-in version
+and if they make sense, check in *both the requirements.txt and the requirements.in file(s).
+
+To try and upgrade a given package also do
+
+    python dev-tools\compile-requirements-txts.py -P <your-package>
+
+To be able to run this script for the first time you need to have installed the development requirements by doing
+
+    pip install -r requirements\dev\requirements.txt
+"""
+
+import subprocess as sp
+import sys
+import pathlib as pl
+
+args = ['pip-compile', *sys.argv[1:]]
+
+DIRS = [
+    ".",
+    "requirements/dev",
+    "requirements/test"
+]
+
+
+def getDirPaths():
+    topLevelDir = pl.Path(__file__).parent.parent
+    for d in DIRS:
+        yield topLevelDir / d
+
+
+print(f"Going to call \"{' '.join(args)}\" for each requirements.in file.")
+for d in getDirPaths():
+    print(f"Compiling {d / 'requirements.txt'}...")
+    sp.run(args, cwd=d)
