5f98e848258591d7f159e1631ac36096e2343d28
Damian Birchler <damian.birchler@ost.ch>
2021-03-08 20:00:31 +0100

    Add more test cases.

--- tests/pytrnsys/psim/testConditionHandler.py
+++ tests/pytrnsys/psim/testConditionHandler.py
@@ -13,6 +13,10 @@ class TestConditionHandler:
         ["serializedConditions", "resultsDict", "areConditionsFulfilled"],
         [
             [["city=BAS", "30<sizeHpUsed<50", "VIceS>28"], dict(city="BAS", sizeHpUsed=44.064, VIceS=28.0676), True],
+            [["city=ZRH", "30<sizeHpUsed<50", "VIceS>28"], dict(city="BAS", sizeHpUsed=44.064, VIceS=28.0676), False],
+            [["city=BAS", "30<sizeHpUsed<50", "VIceS>28"], dict(city="BAS", sizeHpUsed=30, VIceS=28.0676), False],
+            [["city=BAS", "intValue=7", "VIceS>28"], dict(city="BAS", intValue=7, VIceS=28.0676), True],
+            [["city=BAS", "intValue=7", "VIceS>28"], dict(city="BAS", intValue=8, VIceS=28.0676), False],
         ])
     def test(self, serializedConditions: tp.Sequence[str],
              resultsDict: ResultsDict,
