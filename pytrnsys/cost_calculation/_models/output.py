e41adf603aec60b591ebcc2193b45815ded57256
Damian Birchler <damian.birchler@ost.ch>
2021-03-23 10:34:35 +0100

    Add alias for module `uncertainFloat`.

--- pytrnsys/cost_calculation/_models/output.py
+++ pytrnsys/cost_calculation/_models/output.py
@@ -8,7 +8,8 @@ __all__ = ['Output',
 import dataclasses as _dc
 import typing as _tp
 
-import pytrnsys.utils.uncertainFloat
+import pytrnsys.utils.uncertainFloat as _uf
+
 from . import input as _input
 from . import common as _common
 from .. import _economicFunctions as _ef
@@ -39,7 +40,7 @@ class Output:
                                                        parameters.analysisPeriod,
                                                        parameters.maintenanceRate)
 
-        costResidual = pytrnsys.utils.uncertainFloat.UncertainFloat.create(parameters.costResidual)
+        costResidual = _uf.UncertainFloat.create(parameters.costResidual)
         residualCost = ResidualCost(costResidual, parameters.rate, parameters.lifetimeResVal,
                                     parameters.analysisPeriod)
 
@@ -54,7 +55,7 @@ class Output:
         return output
 
     @property
-    def annuity(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def annuity(self) -> _uf.UncertainFloat:
         return self.componentGroups.annuity \
                + self.componentGroups.maintenanceCost \
                + self.yearlyCosts.cost \
@@ -62,7 +63,7 @@ class Output:
                - self.residualCost.annuity
 
     @property
-    def npvCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvCost(self) -> _uf.UncertainFloat:
         return self.componentGroups.cost \
                + self.componentGroups.npvMaintenanceCost \
                + self.yearlyCosts.npvCost \
@@ -70,7 +71,7 @@ class Output:
                - self.residualCost.npvResidualValue
 
     @property
-    def heatGenerationCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def heatGenerationCost(self) -> _uf.UncertainFloat:
         return self.annuity / self.heatingDemandInKWh
 
 
@@ -80,26 +81,26 @@ class ComponentGroups:
 
     @staticmethod
     def createFromValues(definitions: _tp.Sequence[_input.ComponentGroup], values: Values,
-                         rate: pytrnsys.utils.uncertainFloat.UncertainFloat, analysisPeriod: float, maintenanceRate: pytrnsys.utils.uncertainFloat.UncertainFloat) \
+                         rate: _uf.UncertainFloat, analysisPeriod: float, maintenanceRate: _uf.UncertainFloat) \
             -> "ComponentGroups":
         componentGroups = [ComponentGroup.createFromValues(g, values, rate, analysisPeriod, maintenanceRate)
                            for g in definitions]
         return ComponentGroups(componentGroups)
 
     @property
-    def cost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def cost(self) -> _uf.UncertainFloat:
         return sum(g.components.cost for g in self.groups)
 
     @property
-    def annuity(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def annuity(self) -> _uf.UncertainFloat:
         return sum(g.components.annuity for g in self.groups)
 
     @property
-    def maintenanceCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def maintenanceCost(self) -> _uf.UncertainFloat:
         return sum(g.components.maintenanceCost for g in self.groups)
     
     @property
-    def npvMaintenanceCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvMaintenanceCost(self) -> _uf.UncertainFloat:
         return sum(g.components.npvMaintenanceCost for g in self.groups)
 
 
@@ -110,7 +111,7 @@ class ComponentGroup:
 
     @staticmethod
     def createFromValues(definition: _input.ComponentGroup, values: Values,
-                         rate: pytrnsys.utils.uncertainFloat.UncertainFloat, analysisPeriod: float, maintenanceRate: pytrnsys.utils.uncertainFloat.UncertainFloat)\
+                         rate: _uf.UncertainFloat, analysisPeriod: float, maintenanceRate: _uf.UncertainFloat)\
             -> "ComponentGroup":
         costFactors = CostFactors.createForComponentGroup(definition, values, rate, analysisPeriod, maintenanceRate)
 
@@ -123,8 +124,8 @@ class CostFactors:
 
     @staticmethod
     def createForYearlyCosts(definitions: _tp.Sequence[_input.YearlyCost], values: Values,
-                             rate: pytrnsys.utils.uncertainFloat.UncertainFloat, analysisPeriod: float,
-                             maintenanceRate: pytrnsys.utils.uncertainFloat.UncertainFloat) \
+                             rate: _uf.UncertainFloat, analysisPeriod: float,
+                             maintenanceRate: _uf.UncertainFloat) \
             -> "CostFactors":
         costFactors = [_createCostFactor(d, values, rate, analysisPeriod, analysisPeriod, maintenanceRate)
                        for d in definitions]
@@ -132,31 +133,31 @@ class CostFactors:
 
     @staticmethod
     def createForComponentGroup(definition: _input.ComponentGroup, values: Values,
-                                rate: pytrnsys.utils.uncertainFloat.UncertainFloat, analysisPeriod: float,
-                                maintenanceRate: pytrnsys.utils.uncertainFloat.UncertainFloat) \
+                                rate: _uf.UncertainFloat, analysisPeriod: float,
+                                maintenanceRate: _uf.UncertainFloat) \
             -> "CostFactors":
         yearlyCosts = [_createCostFactor(d, values, rate, d.lifetimeInYears, analysisPeriod, maintenanceRate)
                        for d in definition.components]
         return CostFactors(yearlyCosts)
 
     @property
-    def cost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def cost(self) -> _uf.UncertainFloat:
         return sum(f.cost for f in self.factors)
 
     @property
-    def npvCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvCost(self) -> _uf.UncertainFloat:
         return sum(f.npvCost for f in self.factors)
 
     @property
-    def annuity(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def annuity(self) -> _uf.UncertainFloat:
         return sum(f.annuity for f in self.factors)
 
     @property
-    def maintenanceCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def maintenanceCost(self) -> _uf.UncertainFloat:
         return sum(f.maintenanceCost for f in self.factors)
     
     @property
-    def npvMaintenanceCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvMaintenanceCost(self) -> _uf.UncertainFloat:
         return sum(f.npvMaintenanceCost for f in self.factors)
 
 
@@ -180,38 +181,38 @@ def _createCostFactor(inputFactor: _input.CostFactor, values, rate, lifetime, pe
 class CostFactor:
     name: str
     coeffs: _common.LinearCoefficients
-    rate: pytrnsys.utils.uncertainFloat.UncertainFloat
+    rate: _uf.UncertainFloat
     lifetimeInYears: float
     analysisPeriodInYears: float
-    maintenanceRate: pytrnsys.utils.uncertainFloat.UncertainFloat
+    maintenanceRate: _uf.UncertainFloat
     value: "Value"
 
     @property
-    def cost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def cost(self) -> _uf.UncertainFloat:
         return self.coeffs.offset + self.coeffs.slope * self.value.value
 
     @property
-    def npvCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvCost(self) -> _uf.UncertainFloat:
         return self.npvFactor * self.cost
 
     @property
-    def annuity(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def annuity(self) -> _uf.UncertainFloat:
         return self.annuityFactor * self.cost
 
     @property
-    def maintenanceCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def maintenanceCost(self) -> _uf.UncertainFloat:
         return self.maintenanceRate * self.cost
 
     @property
-    def npvMaintenanceCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvMaintenanceCost(self) -> _uf.UncertainFloat:
         return self.npvFactor * self.maintenanceCost
 
     @property
-    def npvFactor(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvFactor(self) -> _uf.UncertainFloat:
         return _ef.getNPV(self.rate, self.analysisPeriodInYears)
 
     @property
-    def annuityFactor(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def annuityFactor(self) -> _uf.UncertainFloat:
         return _ef.getAnnuity(self.rate, self.lifetimeInYears)
 
 
@@ -224,58 +225,58 @@ class Value:
 @_dc.dataclass(frozen=True)
 class Electricity:
     electricityDemandInKWh: float
-    costElecFix: pytrnsys.utils.uncertainFloat.UncertainFloat
-    costElecKWh: pytrnsys.utils.uncertainFloat.UncertainFloat
+    costElecFix: _uf.UncertainFloat
+    costElecKWh: _uf.UncertainFloat
 
-    increaseElecCost: pytrnsys.utils.uncertainFloat.UncertainFloat
-    rate: pytrnsys.utils.uncertainFloat.UncertainFloat
+    increaseElecCost: _uf.UncertainFloat
+    rate: _uf.UncertainFloat
     analysisPeriod: float
 
     @property
-    def cost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def cost(self) -> _uf.UncertainFloat:
         return self.costElecFix + self.costElecKWh * self.electricityDemandInKWh
 
     @property
-    def npvCost(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvCost(self) -> _uf.UncertainFloat:
         return self.cost * self.nvpFactor
 
     @property
-    def annuity(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def annuity(self) -> _uf.UncertainFloat:
         return self.annuityFactor * self.npvCost
 
     @property
-    def nvpFactor(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def nvpFactor(self) -> _uf.UncertainFloat:
         if self.rate == self.increaseElecCost:
             return self.analysisPeriod / (1 + self.rate)
 
         return _ef.getNPVIncreaseCost(self.rate, self.analysisPeriod, self.increaseElecCost)
 
     @property
-    def annuityFactor(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def annuityFactor(self) -> _uf.UncertainFloat:
         return _ef.getAnnuity(self.rate, self.analysisPeriod)
 
 
 @_dc.dataclass(frozen=True)
 class ResidualCost:
-    value: pytrnsys.utils.uncertainFloat.UncertainFloat
-    rate: pytrnsys.utils.uncertainFloat.UncertainFloat
+    value: _uf.UncertainFloat
+    rate: _uf.UncertainFloat
     lifetimeInYears: float
     analysisPeriod: float
 
     @property
-    def residualValue(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def residualValue(self) -> _uf.UncertainFloat:
         residualValueFactor = (self.lifetimeInYears - self.analysisPeriod) / self.lifetimeInYears
         residualValue = residualValueFactor * self.value
         return residualValue
 
     @property
-    def npvResidualValue(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def npvResidualValue(self) -> _uf.UncertainFloat:
         discountFromEnd = (1 + self.rate) ** (-1. * self.analysisPeriod)
         npvResidualValue = discountFromEnd * self.residualValue
         return npvResidualValue
 
     @property
-    def annuity(self) -> pytrnsys.utils.uncertainFloat.UncertainFloat:
+    def annuity(self) -> _uf.UncertainFloat:
         annuityFactor = _ef.getAnnuity(self.rate, self.analysisPeriod)
         annuity = annuityFactor * self.npvResidualValue
         return annuity
