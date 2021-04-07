f090a2850613a59ea4e638dd6b3706eaed1c367f
Damian Birchler <damian.birchler@ost.ch>
2021-04-07 16:04:29 +0200

    Add test for not exporting uncertainties.

--- tests/pytrnsys/plot/comparison/testGle.py
+++ tests/pytrnsys/plot/comparison/testGle.py
@@ -13,6 +13,7 @@ import pytrnsys.plot.comparison._common as _com
 class TestCase:
     allSeries: _tp.Sequence[_com.Series]
     chunkVariable: _tp.Optional[str]
+    shallPlotUncertainties: bool
     expectedOutput: str
 
 
@@ -107,6 +108,7 @@ def generateTestCases():
                 ),
             ],
             None,
+            True,
             """! energyCost=energyCost(AcollR_j, ViceR)
 ! AcollR_1-	AcollR_1=	AcollR_1+	energyCost-(AcollR_1,0.2)	energyCost=(AcollR_1,0.2)	energyCost+(AcollR_1,0.2)	AcollR_2-	AcollR_2=	AcollR_2+	energyCost-(AcollR_2,0.4)	energyCost=(AcollR_2,0.4)	energyCost+(AcollR_2,0.4)	AcollR_3-	AcollR_3=	AcollR_3+	energyCost-(AcollR_3,0.6)	energyCost=(AcollR_3,0.6)	energyCost+(AcollR_3,0.6)	AcollR_4-	AcollR_4=	AcollR_4+	energyCost-(AcollR_4,0.8)	energyCost=(AcollR_4,0.8)	energyCost+(AcollR_4,0.8)	AcollR_5-	AcollR_5=	AcollR_5+	energyCost-(AcollR_5,1.0)	energyCost=(AcollR_5,1.0)	energyCost+(AcollR_5,1.0)
   1.0000	  1.0000	  1.0000	  0.0838	  0.0891	  0.0942	  1.0000	  1.0000	  1.0000	  0.0880	  0.0950	  0.1015	  1.0000	  1.0000	  1.0000	  0.0921	  0.1009	  0.1088	  1.0000	  1.0000	  1.0000	  0.0963	  0.1068	  0.1161	  1.0000	  1.0000	  1.0000	  0.1004	  0.1126	  0.1235	
@@ -115,7 +117,109 @@ def generateTestCases():
   2.5000	  2.5000	  2.5000	  0.1466	  0.1519	  0.1569	  2.5000	  2.5000	  2.5000	  0.1506	  0.1577	  0.1641	  2.5000	  2.5000	  2.5000	  0.1548	  0.1635	  0.1714	  2.5000	  2.5000	  2.5000	  0.1589	  0.1694	  0.1787	  2.5000	  2.5000	  2.5000	  0.1630	  0.1752	  0.1861	
 """,
         ),
-        id="withoutChunks",
+        id="withUncertaintiesWithoutChunks",
+    )
+
+    yield _pt.param(
+        TestCase(
+            [
+                _com.Series(
+                    1,
+                    _com.GroupingValue("ViceR", 0.2),
+                    _com.AxisValues(
+                        "AcollR",
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                    ),
+                    _com.AxisValues(
+                        "energyCost",
+                        _np.array([0.0838, 0.1048, 0.1257, 0.1466]),
+                        _np.array([0.0891, 0.1101, 0.1310, 0.1519]),
+                        _np.array([0.0942, 0.1151, 0.1360, 0.1569]),
+                    ),
+                    False,
+                ),
+                _com.Series(
+                    2,
+                    _com.GroupingValue("ViceR", 0.4),
+                    _com.AxisValues(
+                        "AcollR",
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                    ),
+                    _com.AxisValues(
+                        "energyCost",
+                        _np.array([0.0880, 0.1089, 0.1297, 0.1506]),
+                        _np.array([0.0950, 0.1159, 0.1367, 0.1577]),
+                        _np.array([0.1015, 0.1224, 0.1432, 0.1641]),
+                    ),
+                    False,
+                ),
+                _com.Series(
+                    3,
+                    _com.GroupingValue("ViceR", 0.6),
+                    _com.AxisValues(
+                        "AcollR",
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                    ),
+                    _com.AxisValues(
+                        "energyCost",
+                        _np.array([0.0921, 0.1129, 0.1338, 0.1548]),
+                        _np.array([0.1009, 0.1217, 0.1426, 0.1635]),
+                        _np.array([0.1088, 0.1296, 0.1505, 0.1714]),
+                    ),
+                    False,
+                ),
+                _com.Series(
+                    4,
+                    _com.GroupingValue("ViceR", 0.8),
+                    _com.AxisValues(
+                        "AcollR",
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                    ),
+                    _com.AxisValues(
+                        "energyCost",
+                        _np.array([0.0963, 0.1171, 0.1379, 0.1589]),
+                        _np.array([0.1068, 0.1276, 0.1484, 0.1694]),
+                        _np.array([0.1161, 0.1369, 0.1578, 0.1787]),
+                    ),
+                    False,
+                ),
+                _com.Series(
+                    5,
+                    _com.GroupingValue("ViceR", 1.0),
+                    _com.AxisValues(
+                        "AcollR",
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                        _np.array([1.0, 1.5, 2.0, 2.5]),
+                    ),
+                    _com.AxisValues(
+                        "energyCost",
+                        _np.array([0.1004, 0.1212, 0.1421, 0.1630]),
+                        _np.array([0.1126, 0.1334, 0.1543, 0.1752]),
+                        _np.array([0.1235, 0.1442, 0.1651, 0.1861]),
+                    ),
+                    False,
+                ),
+            ],
+            None,
+            False,
+            """! energyCost=energyCost(AcollR_j, ViceR)
+! AcollR_1	energyCost(AcollR_1,0.2)	AcollR_2	energyCost(AcollR_2,0.4)	AcollR_3	energyCost(AcollR_3,0.6)	AcollR_4	energyCost(AcollR_4,0.8)	AcollR_5	energyCost(AcollR_5,1.0)
+  1.0000	  0.0891	  1.0000	  0.0950	  1.0000	  0.1009	  1.0000	  0.1068	  1.0000	  0.1126	
+  1.5000	  0.1101	  1.5000	  0.1159	  1.5000	  0.1217	  1.5000	  0.1276	  1.5000	  0.1334	
+  2.0000	  0.1310	  2.0000	  0.1367	  2.0000	  0.1426	  2.0000	  0.1484	  2.0000	  0.1543	
+  2.5000	  0.1519	  2.5000	  0.1577	  2.5000	  0.1635	  2.5000	  0.1694	  2.5000	  0.1752	
+""",
+        ),
+        id="withoutUncertaintiesWithoutChunks",
     )
 
     chunk1 = _com.Chunk(
@@ -225,6 +329,7 @@ def generateTestCases():
         TestCase(
             [*chunk1.allSeries, *chunk2.allSeries],
             "chunkVariable",
+            True,
             """! energyCost=energyCost(AcollR_j, ViceR, chunkVariable)
 ! AcollR_1-	AcollR_1=	AcollR_1+	energyCost-(AcollR_1,0.2,-50.333)	energyCost=(AcollR_1,0.2,-50.333)	energyCost+(AcollR_1,0.2,-50.333)	AcollR_2-	AcollR_2=	AcollR_2+	energyCost-(AcollR_2,0.4,-50.333)	energyCost=(AcollR_2,0.4,-50.333)	energyCost+(AcollR_2,0.4,-50.333)	AcollR_3-	AcollR_3=	AcollR_3+	energyCost-(AcollR_3,0.6,-50.333)	energyCost=(AcollR_3,0.6,-50.333)	energyCost+(AcollR_3,0.6,-50.333)	AcollR_4-	AcollR_4=	AcollR_4+	energyCost-(AcollR_4,0.8,50.666)	energyCost=(AcollR_4,0.8,50.666)	energyCost+(AcollR_4,0.8,50.666)	AcollR_5-	AcollR_5=	AcollR_5+	energyCost-(AcollR_5,1.0,50.666)	energyCost=(AcollR_5,1.0,50.666)	energyCost+(AcollR_5,1.0,50.666)
   1.0000	  1.0000	  1.0000	  0.0838	  0.0891	  0.0942	  1.0000	  1.0000	  1.0000	  0.0880	  0.0950	  0.1015	  1.0000	  1.0000	  1.0000	  0.0921	  0.1009	  0.1088	  1.0000	  1.0000	  1.0000	  0.0963	  0.1068	  0.1161	  1.0000	  1.0000	  1.0000	  0.1004	  0.1126	  0.1235	
@@ -233,7 +338,7 @@ def generateTestCases():
   2.5000	  2.5000	  2.5000	  0.1466	  0.1519	  0.1569	  2.5000	  2.5000	  2.5000	  0.1506	  0.1577	  0.1641	  2.5000	  2.5000	  2.5000	  0.1548	  0.1635	  0.1714	-	-	-	-	-	-	  2.5000	  2.5000	  2.5000	  0.1630	  0.1752	  0.1861	
 """,
         ),
-        id="withChunks",
+        id="withUncertaintiesWithChunks",
     )
 
 
@@ -249,7 +354,7 @@ class TestGle:
             "energyCost",
             "ViceR",
             testCase.chunkVariable,
-            True,
+            testCase.shallPlotUncertainties,
         )
 
         actualOutputPath = tmp_path / f"{outputFileStem}.dat"
