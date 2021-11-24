c3b2051f5b81b07fcb005e0ebb8c40a2d6a91910
Martin Neugebauer <martin.neugebauer@ost.ch>
2021-11-24 17:00:29 +0100

    Give warning for "completeFolder" when no lst files are found

--- pytrnsys/psim/processParallelTrnsys.py
+++ pytrnsys/psim/processParallelTrnsys.py
@@ -127,10 +127,7 @@ class ProcessParallelTrnsys:
         self.defaultInputs()
         self.filteredfolder = [".gle"]
 
-        try:
-            self.logger = logging.getself.logger("root")
-        except:
-            self.logger = log.setup_custom_logger("root", self.inputs["outputLevel"])
+        self.logger = log.setup_custom_logger("root", self.inputs["outputLevel"])
 
     def defaultInputs(self):
 
@@ -186,6 +183,7 @@ class ProcessParallelTrnsys:
         self.configPath = path
         tool = readConfig.ReadConfigTrnsys()
         tool.readFile(path, name, self.inputs, parseFileCreated=parseFileCreated)
+        self.logger.setLevel(self.inputs["outputLevel"])
         if "latexNames" in self.inputs.keys():
             if ":" not in self.inputs["latexNames"]:
                 self.inputs["latexNames"] = os.path.join(self.configPath, self.inputs["latexNames"])
@@ -254,6 +252,8 @@ class ProcessParallelTrnsys:
 
             if self.inputs["typeOfProcess"] == "completeFolder":
                 files = glob.glob(os.path.join(pathFolder, "**/*.lst"), recursive=True)
+                if not files:
+                    self.logger.warning("No lst files in %s", pathFolder)
                 fileName = [_pl.Path(name).parts[-2] for name in files]
                 relPaths = [os.path.relpath(os.path.dirname(file), pathFolder) for file in files]
 
