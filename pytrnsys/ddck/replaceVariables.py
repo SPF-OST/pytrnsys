0640ecdefd0c0d11fac0665fce7e95f6fb747268
YongHao1999 <yong_hao1999@hotmail.com>
2022-01-28 12:01:14 +0800

    Can replace new expressions with default values

--- pytrnsys/ddck/replaceVariables.py
+++ pytrnsys/ddck/replaceVariables.py
@@ -1,16 +1,33 @@
-import typing as _tp
 import pathlib as _pl
-import shutil as _sh
+import re as _re
+import typing as _tp
 
 import lark as _lark
 import lark.visitors as _lvis
+
+import pytrnsys.ddck.parse as _parse
 
 
 def replaceComputedVariablesWithDefaults(
         inputDdckFilePath: _pl.Path, outputDdckFilePath: _pl.Path
 ) -> None:
-    _sh.copy(inputDdckFilePath, outputDdckFilePath)
+    tree = _parse.parseDdck(inputDdckFilePath)
 
+    visitor = _CollectAllVariableNamesVisitor()
+
+    visitor.computed_var(tree)
+
+    inputDdckFile = open(inputDdckFilePath, "rt")
+
+    outputDdckFile = open(outputDdckFilePath, "wt")
+
+    for line in inputDdckFile:
+        matching = [defaultPortName for defaultPortName in visitor.variableNames if defaultPortName in line]
+        if len(matching) > 0:
+            replace = _re.sub(r'@.+?[)]', matching[0], line)
+            outputDdckFile.write(replace)
+        else:
+            outputDdckFile.write(line)
 
 class _CollectAllVariableNamesVisitor(_lvis.Visitor_Recursive):
     def __init__(self):
@@ -26,13 +43,11 @@ class _CollectAllVariableNamesVisitor(_lvis.Visitor_Recursive):
         self._addVariableName(tree)
 
     def _addVariableName(self, tree: _lark.Tree) -> None:
-        children = tree.children
-        assert len(children) == 1
-        child = children[0]
 
-        assert isinstance(child, _lark.Token)
-        token = child
-
-        variableName = token.value
-
-        self._variableNames.add(variableName)
+        "Visits the tree, starting with the leaves and finally the root (bottom-up)"
+        for child in tree.children:
+            if tree.data == "computer_var":
+                token = tree.children[-1]
+                self._variableNames.add(token.value)
+            elif isinstance(child, _lark.Tree):
+                self._addVariableName(child)
