9a29a42782ccab59ee24c785ffabbdfc56bccb46
Damian Birchler <damian.birchler@ost.ch>
2022-02-03 13:08:00 +0100

    Replace computed variables using positions provided by lark.

--- pytrnsys/ddck/replaceVariables.py
+++ pytrnsys/ddck/replaceVariables.py
@@ -1,5 +1,5 @@
+import dataclasses as _dc
 import pathlib as _pl
-import re as _re
 import typing as _tp
 
 import lark as _lark
@@ -8,48 +8,74 @@ import lark.visitors as _lvis
 import pytrnsys.ddck.parse as _parse
 
 
-def replaceComputedVariablesWithDefaults(
-        inputDdckFilePath: _pl.Path, outputDdckFilePath: _pl.Path
-) -> None:
-    tree = _parse.parseDdck(inputDdckFilePath)
+@_dc.dataclass
+class _ComputedVariable:
+    startIndex: int
+    endIndex: int
+    defaultVariableName: str
 
-    visitor = _CollectAllVariableNamesVisitor()
-
-    visitor.computed_var(tree)
-
-    inputContent = inputDdckFilePath.read_text()
-
-    copyContent = inputContent
-
-    with open(outputDdckFilePath, "wt") as outputDdckFile:
-        for _unused in range(len(visitor.variableNames)):
-            search = _re.search(r'[^!*](@.+?[)])', copyContent, flags=_re.DOTALL)
-            if search is not None:
-                filteredSearch = _re.search(r'(@.+?[)])', search.group(0), flags=_re.DOTALL)
-                if filteredSearch is not None:
-                    matching = [defaultPortName for defaultPortName in visitor.variableNames if
-                                defaultPortName in filteredSearch.group(0)]
-                    copyContent = copyContent.replace(filteredSearch.group(0), matching[0])
-        outputDdckFile.write(copyContent)
+    @property
+    def lengthChange(self) -> int:
+        lengthBeforeReplacing = self.endIndex - self.startIndex
+        lengthAfterReplacing = len(self.defaultVariableName)
+        lengthChange = lengthAfterReplacing - lengthBeforeReplacing
+        return lengthChange
 
 
-class _CollectAllVariableNamesVisitor(_lvis.Visitor_Recursive):
+class _CollectComputedVariables(_lvis.Visitor_Recursive):
     def __init__(self):
         super().__init__()
 
-        self._variableNames = set()
-
-    @property
-    def variableNames(self) -> _tp.Sequence[str]:
-        return sorted(self._variableNames)
+        self.computedVariables = []
 
     def computed_var(self, tree: _lark.Tree) -> None:  # pylint: disable=invalid-name
-        self._addVariableName(tree)
+        defaultVariableName = self._getChildToken("DEFAULT_VARIABLE_NAME", tree)
+        computedVariable = _ComputedVariable(tree.meta.start_pos, tree.meta.end_pos, defaultVariableName)
+        self.computedVariables.append(computedVariable)
 
-    def _addVariableName(self, tree: _lark.Tree) -> None:
-        for child in tree.children:
-            if tree.data == "computed_var":
-                token = tree.children[-1]
-                self._variableNames.add(token)
-            elif isinstance(child, _lark.Tree):
-                self._addVariableName(child)
+    @staticmethod
+    def _getChildToken(tokenType: str, tree: _lark.Tree) -> str:  # pylint: disable=invalid-name
+        matchingChildTokens = [c for c in tree.children if isinstance(c, _lark.Token) and c.type == tokenType]
+
+        if len(matchingChildTokens) != 1:
+            raise ValueError(f"None or more than one token of type {tokenType} found.")
+
+        matchingChildToken = matchingChildTokens[0]
+
+        return matchingChildToken.value
+
+
+def replaceComputedVariablesWithDefaults(inputDdckFilePath: _pl.Path, outputDdckFilePath: _pl.Path) -> None:
+    computedVariables = _getComputedVariablesSortedByStartIndexAscending(inputDdckFilePath)
+
+    inputDdckContent = inputDdckFilePath.read_text()
+
+    outputDdckContent = inputDdckContent
+    offset = 0
+    for computedVariable in computedVariables:
+        outputDdckContent = _replace(
+            outputDdckContent,
+            offset + computedVariable.startIndex,
+            offset + computedVariable.endIndex,
+            computedVariable.defaultVariableName,
+        )
+        offset += computedVariable.lengthChange
+
+    outputDdckFilePath.write_text(outputDdckContent)
+
+
+def _getComputedVariablesSortedByStartIndexAscending(inputDdckFilePath: _pl.Path) -> _tp.Sequence["_ComputedVariable"]:
+    tree = _parse.parseDdck(inputDdckFilePath)
+    visitor = _CollectComputedVariables()
+    visitor.visit(tree)
+
+    sortedComputedVariables = sorted(visitor.computedVariables, key=lambda v: v.startIndex)
+
+    currentAndNext = zip(sortedComputedVariables[:-1], sortedComputedVariables[1:])
+    assert all(n.startIndex >= c.endIndex for c, n in currentAndNext)
+
+    return sortedComputedVariables
+
+
+def _replace(content: str, startIndex: int, endIndex: int, replacement: str) -> str:
+    return content[:startIndex] + replacement + content[endIndex:]
