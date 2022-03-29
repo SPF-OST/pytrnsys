44df7e13f76dbe31dcaaacf80d45e587e75a365b
Damian Birchler <damian.birchler@ost.ch>
2022-03-29 18:26:54 +0200

    Compare module user-specific paths.

--- tests/pytrnsys/rsim/testRunParallelTrnsys.py
+++ tests/pytrnsys/rsim/testRunParallelTrnsys.py
@@ -1,11 +1,20 @@
 import inspect as _is
 import pathlib as _pl
 
-import jinja2 as _ja
 import pytest as _pytest
 
 import pytrnsys as _pt
 import pytrnsys.rsim.runParallelTrnsys as _rpt
+
+
+def _getPytrnsysSourceDirPath() -> _pl.Path:
+    pytrnsysModuleSourceFilePathAsString = _is.getsourcefile(_pt)
+    assert pytrnsysModuleSourceFilePathAsString
+    pytrnsysModuleDirPath = _pl.Path(pytrnsysModuleSourceFilePathAsString).parents[1]
+    return pytrnsysModuleDirPath
+
+
+PYTRNSYS_SOURCE_DIR_PATH = _getPytrnsysSourceDirPath()
 
 
 class TestRunParallelTrnsys:  # pylint: disable=too-few-public-methods
@@ -13,41 +22,45 @@ class TestRunParallelTrnsys:  # pylint: disable=too-few-public-methods
     def testRunConfig(self):
         testDirPath = _pl.Path(__file__).parent
 
-        inputDirPath = testDirPath / "data" / "input" / "05_pytrnsys_Files"
-        expectedDckFilePath = testDirPath / "data" / "expected"
+        inputDirPath = testDirPath / "data" / "input"
+        pytrnsysFilesDirPath = inputDirPath / "05_pytrnsys_Files"
+        expectedDirPath = testDirPath / "data" / "expected"
 
         self._createConfigFile(inputDirPath)
 
-        inputDirPathAsString = str(inputDirPath)
+        pytrnsysFilesDirAsString = str(pytrnsysFilesDirPath)
 
-        runParallelTrnsys = _rpt.RunParallelTrnsys(pathConfig=inputDirPathAsString)
-        runParallelTrnsys.path = inputDirPathAsString
-        runParallelTrnsys.readConfig(inputDirPathAsString, "Building_4Study.config")
+        runParallelTrnsys = _rpt.RunParallelTrnsys(pathConfig=pytrnsysFilesDirAsString)
+        runParallelTrnsys.path = pytrnsysFilesDirAsString
+        runParallelTrnsys.readConfig(pytrnsysFilesDirAsString, "Building_4Study.config")
         runParallelTrnsys.getConfig()
         runParallelTrnsys.overwriteForcedByUser = False
 
         runParallelTrnsys.runConfig()
 
-        expectedDckFilePath = expectedDckFilePath / "pytrnsysRun.dck"
-        actualDckFilePath = inputDirPath / "Building_4Study" / "pytrnsysRun" / "pytrnsysRun.dck"
+        actualDckFilePath = pytrnsysFilesDirPath / "Building_4Study" / "pytrnsysRun" / "pytrnsysRun.dck"
+        expectedDdckFileContent = self._getExpectedDckFileContent(expectedDirPath)
 
-        assert actualDckFilePath.read_text(encoding="windows-1252") == expectedDckFilePath.read_text(
-            encoding="windows-1252"
-        )
+        assert actualDckFilePath.read_text(encoding="windows-1252") == expectedDdckFileContent
 
     @staticmethod
     def _createConfigFile(inputDirPath: _pl.Path) -> None:
-        configFileTemplatePath = inputDirPath / "Building_4Study.config.jinja"
-
-        pytrnsysModuleSourceFilePathAsString = _is.getsourcefile(_pt)
-        assert pytrnsysModuleSourceFilePathAsString
-
-        pytrnsysModuleDirPath = _pl.Path(pytrnsysModuleSourceFilePathAsString).parent
-        pytrnsysDdcksPath = pytrnsysModuleDirPath.parent / "data" / "ddcks"
+        configFileTemplatePath = inputDirPath / "05_pytrnsys_Files" / "Building_4Study.config.template"
 
         templateContent = configFileTemplatePath.read_text()
-        configFileTemplate = _ja.Template(templateContent)
-        renderedConfigFileContent = configFileTemplate.render(pytrnsys_ddck_folder=str(pytrnsysDdcksPath))
+
+        content = templateContent
+        content = content.replace("$PYTRNSYS_FOLDER", str(PYTRNSYS_SOURCE_DIR_PATH))
+        content = content.replace("$INPUT_FOLDER", str(inputDirPath))
 
         configFilePath = configFileTemplatePath.with_suffix("")
-        configFilePath.write_text(renderedConfigFileContent)
+        configFilePath.write_text(content)
+
+    @staticmethod
+    def _getExpectedDckFileContent(expectedDirPath):
+        expectedDckFileTemplatePath = expectedDirPath / "pytrnsysRun.dck.template"
+        expectedDdckFileTemplateContent = expectedDckFileTemplatePath.read_text(encoding="windows-1252")
+        expectedDdckFileContent = expectedDdckFileTemplateContent.replace(
+            "$PYTRNSYS_FOLDER", str(PYTRNSYS_SOURCE_DIR_PATH)
+        )
+        return expectedDdckFileContent
