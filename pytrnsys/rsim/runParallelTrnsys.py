3a730f12e1e95f4841c92e21071d0d671ca76ec2
Damian Birchler <damian.birchler@ost.ch>
2022-06-07 09:19:47 +0200

    Log deck build errors as we can't be sure clients check return value.

--- pytrnsys/rsim/runParallelTrnsys.py
+++ pytrnsys/rsim/runParallelTrnsys.py
@@ -70,6 +70,7 @@ class RunParallelTrnsys:
             self.path = os.getcwd()
         else:
             self.path = runPath
+
         if configFile is not None:
             self.readConfig(self.pathConfig, configFile)
 
@@ -232,7 +233,9 @@ class RunParallelTrnsys:
                         result = self.buildTrnsysDeck()
 
                         if _res.isError(result):
-                            return _res.error(result)
+                            error = _res.error(result)
+                            self.logger.error(error.message)
+                            return error
 
                         self.createDecksFromVariant()
 
@@ -242,7 +245,9 @@ class RunParallelTrnsys:
                 result = self.buildTrnsysDeck()
 
                 if _res.isError(result):
-                    return _res.error(result)
+                    error = _res.error(result)
+                    self.logger.error(error.message)
+                    return error
 
                 self.createDecksFromVariant()
 
