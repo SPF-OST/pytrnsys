SPF-OST/pytrnsys

commit 4c4c202db39efb7921992183ce0eed6d5924a275

changes necessary to do auto test in fortrnsys

pytrnsys/psim/processTrnsysDf.py
@@ -397,6 +397,11 @@ class ProcessTrnsysDf:
         self.yearlyMax = {value + "_Max": self.houDataDf[value].max() for value in self.houDataDf.columns}
         self.yearlyAvg = {value + "_Avg": self.houDataDf[value].mean() for value in self.houDataDf.columns}
 
+        self.yearlyMin = {value + "_Min": round(self.steDataDf[value].min(),2) for value in self.steDataDf.columns}
+        self.yearlyMax = {value + "_Max": round(self.steDataDf[value].max(),2) for value in self.steDataDf.columns}
+        self.yearlyAvg = {value + "_Avg": round(self.steDataDf[value].mean(),2) for value in self.steDataDf.columns}
+        self.yearlyEnd = {value + "_End": round(self.steDataDf[value][-1],2) for value in self.steDataDf.columns}
+
         for column in self.monDataDf.columns:
             self.monDataDf["Cum_" + column] = self.monDataDf[column].cumsum()
         self.calcConfigEquations()
@@ -408,6 +413,11 @@ class ProcessTrnsysDf:
         self.yearlyMin = {value + "_Min": self.houDataDf[value].min() for value in self.houDataDf.columns}
         self.yearlyMax = {value + "_Max": self.houDataDf[value].max() for value in self.houDataDf.columns}
         self.yearlyAvg = {value + "_Avg": self.houDataDf[value].mean() for value in self.houDataDf.columns}
+
+        self.yearlyMin = {value + "_Min": round(self.steDataDf[value].min(), 2) for value in self.steDataDf.columns}
+        self.yearlyMax = {value + "_Max": round(self.steDataDf[value].max(), 2) for value in self.steDataDf.columns}
+        self.yearlyAvg = {value + "_Avg": round(self.steDataDf[value].mean(), 2) for value in self.steDataDf.columns}
+        self.yearlyEnd = {value + "_End": round(self.steDataDf[value][-1], 2) for value in self.steDataDf.columns}
 
         try:
             self.myShortMonths = utils.getShortMonthyNameArray(self.monDataDf["Month"].values)
@@ -826,6 +836,7 @@ class ProcessTrnsysDf:
                 **self.yearlyMin,
                 **self.yearlyMax,
                 **self.yearlyAvg,
+                **self.cumSumEnd,
             }
             expression = equation.replace(" ", "")
             exec(expression, globals(), namespace)
@@ -859,7 +870,7 @@ class ProcessTrnsysDf:
             self.dayDataDf.eval(equation, inplace=True, **kwargs)
             self.yearlyMin = {value + "_Min": self.dayDataDf[value].min() for value in self.dayDataDf.columns}
             self.yearlyMax = {value + "_Max": self.dayDataDf[value].max() for value in self.dayDataDf.columns}
-            self.cumSumEnd = {value + "_End": self.dayDataDf[value][-1] for value in self.dayDataDf.columns}
+            # self.cumSumEnd = {value + "_End": self.dayDataDf[value][-1] for value in self.dayDataDf.columns} #I guess not needed since we dont have cumsumDaily?
             self.yearlyAvg = {value + "_Avg": self.dayDataDf[value].mean() for value in self.houDataDf.columns}
 
         for equation in self.inputs["calcHourly"]:
@@ -876,7 +887,7 @@ class ProcessTrnsysDf:
             self.houDataDf.eval(equation, inplace=True, **kwargs)
             self.yearlyMin = {value + "_Min": self.houDataDf[value].min() for value in self.houDataDf.columns}
             self.yearlyMax = {value + "_Max": self.houDataDf[value].max() for value in self.houDataDf.columns}
-            self.cumSumEnd = {value + "_End": self.houDataDf[value][-1] for value in self.houDataDf.columns}
+            # self.cumSumEnd = {value + "_End": self.houDataDf[value][-1] for value in self.houDataDf.columns}
             self.yearlyAvg = {value + "_Avg": self.houDataDf[value].mean() for value in self.houDataDf.columns}
 
         for equation in self.inputs["calcMonthlyFromHourly"]:
@@ -900,7 +911,7 @@ class ProcessTrnsysDf:
             self.monDataDf[calculatedVariableName] = calculatedVariablePerMonth
             self.yearlyMin = {value + "_Min": self.houDataDf[value].min() for value in self.houDataDf.columns}
             self.yearlyMax = {value + "_Max": self.houDataDf[value].max() for value in self.houDataDf.columns}
-            self.cumSumEnd = {value + "_End": self.houDataDf[value][-1] for value in self.houDataDf.columns}
+            # self.cumSumEnd = {value + "_End": self.houDataDf[value][-1] for value in self.houDataDf.columns}
             self.yearlyAvg = {value + "_Avg": self.houDataDf[value].mean() for value in self.houDataDf.columns}
 
         for equation in self.inputs["calcCumSumHourly"]:
@@ -910,6 +921,7 @@ class ProcessTrnsysDf:
                         self.houDataDf["cumsum_" + value] = self.houDataDf[value].cumsum()
                         myValue = "cumsum_" + value
                         self.cumSumEnd = {myValue + "_End": self.houDataDf[myValue][-1]}
+
         for equation in self.inputs["calcHourlyTest"]:
             kwargs = {"local_dict": {**self.deckData, **self.yearlySums, **self.yearlyMin, **self.yearlyMax}}
             scalars = kwargs["local_dict"].keys()
@@ -924,10 +936,10 @@ class ProcessTrnsysDf:
             # self.yearlyMax = {value + '_Ma': self.houDataDf[value].max()}
             self.yearlyMin = {value + "_Min": self.houDataDf[value].min() for value in self.houDataDf.columns}
             self.yearlyMax = {value + "_Max": self.houDataDf[value].max() for value in self.houDataDf.columns}
-            self.cumSumEnd = {value + "_End": self.houDataDf[value][-1] for value in self.houDataDf.columns}
+            # self.cumSumEnd = {value + "_End": self.houDataDf[value][-1] for value in self.houDataDf.columns}
             self.yearlyAvg = {value + "_Avg": self.houDataDf[value].mean() for value in self.houDataDf.columns}
 
-        for equation in self.inputs["calcTimeStep"]:
+        for equation in (self.inputs["calcTimeStep"]):
             kwargs = {"local_dict": {**self.deckData, **self.yearlySums, **self.yearlyMin, **self.yearlyMax}}
             scalars = kwargs["local_dict"].keys()
             splitEquation = equation.split("=")
@@ -941,12 +953,16 @@ class ProcessTrnsysDf:
             )  # by doing so we add also the key into the dictionary steDataDf
             self.yearlyMin = {value + "_Min": self.steDataDf[value].min() for value in self.steDataDf.columns}
             self.yearlyMax = {value + "_Max": self.steDataDf[value].max() for value in self.steDataDf.columns}
+            # self.cumSumEnd = {value + "_End": self.steDataDf[value][-1] for value in self.steDataDf.columns}
 
         for equation in self.inputs["calcCumSumTimeStep"]:
             for value in equation:
                 for key in self.steDataDf.columns:
                     if key == value:
-                        self.steDataDf["cumsum_" + value] = self.steDataDf[value].cumsum()
+                        myValue = "cumsum_" + value
+                        self.steDataDf[myValue] = self.steDataDf[value].cumsum()
+                        # self.cumSumEnd = {myValue + "_End": self.steDataDf[myValue][-1]}
+                        self.cumSumEnd.update({myValue + "_End": round(self.steDataDf[myValue][-1],2)})
 
         for equation in self.inputs[
             "calcTimeStepTest"
@@ -971,6 +987,7 @@ class ProcessTrnsysDf:
                 **self.yearlyMin,
                 **self.yearlyMax,
                 **self.yearlyAvg,
+                **self.cumSumEnd,
             }
             expression = equation.replace(" ", "")
             exec(expression, globals(), namespace)
@@ -2035,6 +2052,7 @@ class ProcessTrnsysDf:
                 **self.yearlyMin,
                 **self.yearlyMax,
                 **self.yearlyAvg,
+                **self.yearlyEnd,
                 **self.cumSumEnd,
             }  # ,**self.maximumMonth,**self.minimumMonth}
             for key in self.inputs["results"][0]:

pytrnsys/psim/processParallelTrnsys.py
@@ -168,6 +168,7 @@ class ProcessParallelTrnsys:
         self.inputs["hourlyBalance"] = False
         # self.inputs['daysSelected'] = "2019,2,30" "2019,4,30" "2019,8,30"
 
+        # self.inputs["automaticTrnsysProcess"] = True maybe add this to set to false automatically the ones below
         self.inputs["calculateHeatDemand"] = True
         self.inputs["calculateSPF"] = True
         self.inputs["addWeightedSPF"] = False
@@ -242,6 +243,8 @@ class ProcessParallelTrnsys:
         fileName = []
         classList = []
 
+        self.filesReturn=None
+
         if os.path.exists(os.path.join(self.inputs["pathBase"], "Summary.dat")):
             os.remove(os.path.join(self.inputs["pathBase"], "Summary.dat"))
 
@@ -253,6 +256,8 @@ class ProcessParallelTrnsys:
                 files = glob.glob(os.path.join(pathFolder, "**/*.lst"), recursive=True)
                 fileName = [_pl.Path(name).parts[-2] for name in files]
                 relPaths = [os.path.relpath(os.path.dirname(file), pathFolder) for file in files]
+
+                self.filesReturn = files
 
             elif self.inputs["typeOfProcess"] == "json":
                 files = glob.glob(os.path.join(pathFolder, "**/*.json"), recursive=True)
@@ -311,6 +316,9 @@ class ProcessParallelTrnsys:
             pathFolder = self.inputs["pathBase"]
             baseClass = self.getBaseClass(self.inputs["classProcessing"], pathFolder, name)  # DC This was missing
             casesInputs.append((baseClass, pathFolder, name, self.inputs))  # DC This was missing
+
+            self.filesReturn=[]
+            self.filesReturn.append(os.path.join(pathFolder, name, name)+".dck")
 
         elif self.inputs["typeOfProcess"] == "citiesFolder":
 
@@ -514,6 +522,8 @@ class ProcessParallelTrnsys:
 
         if "printBoxPlotGLEData" in self.inputs.keys():
             self.printBoxPlotGLEData()
+
+        return self.filesReturn #Dc maybe not the best way
 
     def calculationsAcrossSets(self):
         pathFolder = self.inputs["pathBase"]

pytrnsys/rsim/runParallelTrnsys.py
@@ -148,8 +148,9 @@ class RunParallelTrnsys:
         file = open(fileToRunWithPath, "r")
         lines = file.readlines()
         cases = []
+
         for line in lines:
-            if line == "\n":  # ignoring blank lines
+            if line == "\n" or line[0]=="#":  # ignoring blank lines and lines starting with #
                 pass
             else:
                 cases.append(line[:-1])  # remove \n
@@ -171,11 +172,11 @@ class RunParallelTrnsys:
                 tests[i].ignoreOnlinePlotter()
 
             tests[i].setRemovePopUpWindow(self.inputs["removePopUpWindow"])
-            tests[i].copyFilesForRunning()
+            # tests[i].copyFilesForRunning()
 
             # tests[i].setTrnsysVersion("TRNSYS17_EXE")
 
-            self.cmds.append(tests[i].getExecuteTrnsys(self.inputs))
+            self.cmds.append(tests[i].getExecuteTrnsys(self.inputs,useDeckName=tests[i].nameDck))
 
         self.runParallel()
 

commit 10ac10e3b70583a3a2de42a3826cfe7a4d18da5f

Fixed bug introduced in 4c4c202: Yearly values created from hourly values were overwritten with timestep values

pytrnsys/psim/processTrnsysDf.py
@@ -396,27 +396,38 @@ class ProcessTrnsysDf:
         self.yearlyMax = {value + "_Max": self.houDataDf[value].max() for value in self.houDataDf.columns}
         self.yearlyAvg = {value + "_Avg": self.houDataDf[value].mean() for value in self.houDataDf.columns}
 
-        self.yearlyMin = {value + "_Min": round(self.steDataDf[value].min(),2) for value in self.steDataDf.columns}
-        self.yearlyMax = {value + "_Max": round(self.steDataDf[value].max(),2) for value in self.steDataDf.columns}
-        self.yearlyAvg = {value + "_Avg": round(self.steDataDf[value].mean(),2) for value in self.steDataDf.columns}
+        for value in self.steDataDf.columns:
+            self.yearlyMin[value + "_Min"] = round(self.steDataDf[value].min(),2)
+            self.yearlyMax[value + "_Max"] = round(self.steDataDf[value].max(),2)
+            self.yearlyAvg[value + "_Avg"] = round(self.steDataDf[value].mean(),2)
+
         self.yearlyEnd = {value + "_End": round(self.steDataDf[value][-1],2) for value in self.steDataDf.columns}
 
         for column in self.monDataDf.columns:
             self.monDataDf["Cum_" + column] = self.monDataDf[column].cumsum()
         self.calcConfigEquations()
 
-        # We recalculate all to capture also variables that were calculated from equations, while at the same time
-        # having all read-in variables feature _Tot, _Min, _Max and _Avg; should be improved
+        for value in self.monDataDf.columns:
+            if not(value in self.yearlySums):
+                self.yearlySums[value + "_Tot"] = self.monDataDf[value].sum()
 
-        self.yearlySums = {value + "_Tot": self.monDataDf[value].sum() for value in self.monDataDf.columns}
-        self.yearlyMin = {value + "_Min": self.houDataDf[value].min() for value in self.houDataDf.columns}
-        self.yearlyMax = {value + "_Max": self.houDataDf[value].max() for value in self.houDataDf.columns}
-        self.yearlyAvg = {value + "_Avg": self.houDataDf[value].mean() for value in self.houDataDf.columns}
+        for value in self.houDataDf.columns:
+            if not (value in self.yearlyMin):
+                self.yearlyMin[value + "_Min"] = self.houDataDf[value].min()
+                self.yearlyMax[value + "_Max"] = self.houDataDf[value].max()
+                self.yearlyAvg[value + "_Avg"] = self.houDataDf[value].mean()
 
-        self.yearlyMin = {value + "_Min": round(self.steDataDf[value].min(), 2) for value in self.steDataDf.columns}
-        self.yearlyMax = {value + "_Max": round(self.steDataDf[value].max(), 2) for value in self.steDataDf.columns}
-        self.yearlyAvg = {value + "_Avg": round(self.steDataDf[value].mean(), 2) for value in self.steDataDf.columns}
-        self.yearlyEnd = {value + "_End": round(self.steDataDf[value][-1], 2) for value in self.steDataDf.columns}
+        for value in self.steDataDf.columns:
+            if not (value in self.yearlyMin):
+                self.yearlyMin[value + "_Min"] = round(self.steDataDf[value].min(),2)
+                self.yearlyMax[value + "_Max"] = round(self.steDataDf[value].max(),2)
+                self.yearlyAvg[value + "_Avg"] = round(self.steDataDf[value].mean(),2)
+                self.yearlyEnd[value + "_End"] = round(self.steDataDf[value][-1],2)
+
+        variablesWithTimeStepANDHourlyData = list(set(self.houDataDf.columns) & set(self.steDataDf.columns))
+        # for variable in variablesWithTimeStepANDHourlyData:
+        # IMPLEMENT logger warning here about intersection between timestep and hourly variables (logger currently not
+        # imported to the local class.)
 
         try:
             self.myShortMonths = utils.getShortMonthyNameArray(self.monDataDf["Month"].values)