1453412bf87e6a87333a0d62412bc63301e98a5a
Damian Birchler <damian.birchler@ost.ch>
2023-02-09 17:35:35 +0100

    Make log format public.

--- pytrnsys/utils/log.py
+++ pytrnsys/utils/log.py
@@ -3,13 +3,16 @@ import typing as _tp
 import pathlib as _pl
 
 
+FORMAT = "%(asctime)s - %(levelname)s - %(module)s - %(message)s"
+
+
 def getOrCreateCustomLogger(name: str, level: str, logFilePath: _tp.Optional[_pl.Path] = None) -> _log.Logger:
     logger = _log.getLogger(name)
 
     if logger.hasHandlers():
         return logger
 
-    formatter = _log.Formatter(fmt="%(asctime)s - %(levelname)s - %(module)s - %(message)s")
+    formatter = _log.Formatter(fmt=FORMAT)
 
     streamHandler = _log.StreamHandler()
     handlers: list[_log.Handler] = [streamHandler]
