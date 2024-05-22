5815190dd0d7c0b89087e3fcc9172cba0fcb39e5
Damian Birchler <damian.birchler@ost.ch>
2024-05-22 17:25:38 +0200

    Add expected files for new test.

---
+++ tests/pytrnsys/costCalculation/_helper.py
@@ -0,0 +1,71 @@
+import filecmp
+import logging as log
+import pathlib as pl
+import shutil
+
+import pytest
+
+
+class Helper:
+    def __init__(
+        self,
+        dataDirPath: pl.Path,
+        resultsDirName: str,
+        caplog: pytest.LogCaptureFixture,
+        costConfigFileName: str | None = None,
+    ):
+        self._caplog = caplog
+
+        inputDir = dataDirPath / "input"
+        self._resultsDir = inputDir / resultsDirName
+        self.costParametersFilePath = inputDir / costConfigFileName if costConfigFileName else None
+
+        outputDir = dataDirPath / "output"
+        actualDir = outputDir / "actual"
+        self.actualResultsDir = actualDir / resultsDirName
+        expectedDir = outputDir / "expected"
+        self._expectedResultsDir = expectedDir / resultsDirName
+
+    def setup(self):
+        self._setupLogging()
+        self._setupActualDirectory()
+
+    def _setupLogging(self):
+        self._caplog.set_level(log.DEBUG, "root")
+
+    def _setupActualDirectory(self):
+        if self.actualResultsDir.exists():
+            shutil.rmtree(self.actualResultsDir)
+        shutil.copytree(self._resultsDir, self.actualResultsDir)
+
+    def assertResultsAreAsExpected(self):
+        self._assertFileStructureEqual()
+        self._assertOutputFilesEqual()
+
+    def _assertFileStructureEqual(self):
+        dircmp = filecmp.dircmp(self.actualResultsDir, self._expectedResultsDir)
+
+        if True:
+            assert not dircmp.left_only
+            assert not dircmp.right_only
+
+    def _assertOutputFilesEqual(self):
+        for resultsJsonFilePath in self._expectedResultsDir.rglob("*-results.json"):
+            simulationName = resultsJsonFilePath.name[: -len("-results.json")]
+            relativeContainingDirPath = resultsJsonFilePath.parent.relative_to(self._expectedResultsDir)
+
+            reportTexName = f"{simulationName}-cost.tex"
+            self._assertTextFileEqual(relativeContainingDirPath, reportTexName)
+
+            resultJsonName = f"{simulationName}-results.json"
+            self._assertTextFileEqual(relativeContainingDirPath, resultJsonName)
+
+    def _assertTextFileEqual(self, relativeContainingDirPath, texFileName):
+        expectedPath, actualPath = self._getExpectedAndActualPath(relativeContainingDirPath, texFileName)
+        assert actualPath.read_text() == expectedPath.read_text()
+
+    def _getExpectedAndActualPath(self, relativeContainingDirPath, fileName):
+        expected = self._expectedResultsDir / relativeContainingDirPath / fileName
+        actual = self.actualResultsDir / relativeContainingDirPath / fileName
+
+        return expected, actual
--- tests/pytrnsys/costCalculation/testCostCalculation.py
+++ tests/pytrnsys/costCalculation/testCostCalculation.py
@@ -1,14 +1,13 @@
 # pylint: skip-file
 # type: ignore
 
-import filecmp
-import pathlib as pl
-import shutil
-import logging as log
+import pathlib as _pl
 
-import pytest
+import pytest as _pt
 
-import pytrnsys.costCalculation as cc
+import pytrnsys.costCalculation as _cc
+
+from . import _helper
 
 
 class TestCostCalculation:
@@ -18,90 +17,26 @@ class TestCostCalculation:
         ["costSolarIce_HpSplit_typeOfProcess_json.json", "results_json_files"],
     ]
 
-    @pytest.mark.parametrize(["costConfigFileName", "resultsDirName"], CONFIG_FILE_RESULTS_DIR_PAIRS)
+    @_pt.mark.parametrize(["costConfigFileName", "resultsDirName"], CONFIG_FILE_RESULTS_DIR_PAIRS)
     def test(
         self,
         costConfigFileName: str,
         resultsDirName: str,
-        caplog: pytest.LogCaptureFixture,
+        caplog: _pt.LogCaptureFixture,
     ):
-        helper = Helper(costConfigFileName, resultsDirName, caplog)
+
+        dataDirPath = _pl.Path(__file__).parent / "data"
+        helper = _helper.Helper(dataDirPath, resultsDirName, caplog, costConfigFileName)
         helper.setup()
 
         actualResultsDir = helper.actualResultsDir
         costParametersFilePath = helper.costParametersFilePath
 
-        cc.calculateCostsAndWriteReports(
+        _cc.calculateCostsAndWriteReports(
             costParametersFilePath,
             actualResultsDir,
             shallWriteReport=True,
-            processType=cc.OTHER,
+            processType=_cc.OTHER,
         )
 
         helper.assertResultsAreAsExpected()
-
-
-class Helper:
-    def __init__(
-        self,
-        costConfigFileName: str,
-        resultsDirName: str,
-        caplog: pytest.LogCaptureFixture,
-    ):
-        self._caplog = caplog
-
-        baseDir = pl.Path(__file__).parent
-
-        inputDir = baseDir / "input"
-        self._resultsDir = inputDir / resultsDirName
-        self.costParametersFilePath = inputDir / costConfigFileName
-
-        outputDir = baseDir / "output"
-        actualDir = outputDir / "actual"
-        self.actualResultsDir = actualDir / resultsDirName
-        expectedDir = outputDir / "expected"
-        self._expectedResultsDir = expectedDir / resultsDirName
-
-    def setup(self):
-        self._setupLogging()
-        self._setupActualDirectory()
-
-    def _setupLogging(self):
-        self._caplog.set_level(log.DEBUG, "root")
-
-    def _setupActualDirectory(self):
-        if self.actualResultsDir.exists():
-            shutil.rmtree(self.actualResultsDir)
-        shutil.copytree(self._resultsDir, self.actualResultsDir)
-
-    def assertResultsAreAsExpected(self):
-        self._assertFileStructureEqual()
-        self._assertOutputFilesEqual()
-
-    def _assertFileStructureEqual(self):
-        dircmp = filecmp.dircmp(self.actualResultsDir, self._expectedResultsDir)
-
-        if True:
-            assert not dircmp.left_only
-            assert not dircmp.right_only
-
-    def _assertOutputFilesEqual(self):
-        for resultsJsonFilePath in self._expectedResultsDir.rglob("*-results.json"):
-            simulationName = resultsJsonFilePath.name[: -len("-results.json")]
-            relativeContainingDirPath = resultsJsonFilePath.parent.relative_to(self._expectedResultsDir)
-
-            reportTexName = f"{simulationName}-cost.tex"
-            self._assertTextFileEqual(relativeContainingDirPath, reportTexName)
-
-            resultJsonName = f"{simulationName}-results.json"
-            self._assertTextFileEqual(relativeContainingDirPath, resultJsonName)
-
-    def _assertTextFileEqual(self, relativeContainingDirPath, texFileName):
-        expectedPath, actualPath = self._getExpectedAndActualPath(relativeContainingDirPath, texFileName)
-        assert actualPath.read_text() == expectedPath.read_text()
-
-    def _getExpectedAndActualPath(self, relativeContainingDirPath, fileName):
-        expected = self._expectedResultsDir / relativeContainingDirPath / fileName
-        actual = self.actualResultsDir / relativeContainingDirPath / fileName
-
-        return expected, actual
