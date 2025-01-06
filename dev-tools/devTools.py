1cd4f7e415c779012297e8bf0501285be6a53389
Damian Birchler <damian@damianbirchler.net>
2025-01-06 09:52:58 +0100

    Don't check not-existing-anymore `scripts` dir.

--- dev-tools/devTools.py
+++ dev-tools/devTools.py
@@ -13,7 +13,7 @@ import typing as tp
 
 _SCRIPTS_DIR = pl.Path(sc.get_path("scripts"))
 
-_SOURCE_DIRS = ["pytrnsys", "scripts", "tests", "dev-tools"]
+_SOURCE_DIRS = ["pytrnsys", "tests", "dev-tools"]
 
 _EXCLUDED_PATH_PATTERNS = [
     "^tests/(.+/)?data/.*",
