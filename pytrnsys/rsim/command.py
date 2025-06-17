45eb2d218224b65e05bcb8ba89489d7248870020
Damian Birchler <damian@damianbirchler.net>
2025-06-17 15:39:35 +0200

    Root doesn't count toward path length.

--- pytrnsys/rsim/command.py
+++ pytrnsys/rsim/command.py
@@ -8,6 +8,11 @@ import shortpath83 as _sp83
 
 @_dc.dataclass
 class Command:
+    """
+    This class represents a simulation that we are going to run in a separate
+    process with TRNSYS using `subprocess.Popen`.
+    """
+
     MAX_DECK_FILE_PATH_LENGTH: _tp.ClassVar[int] = 260
 
     trnsysExeFilePath: _pl.Path
@@ -23,8 +28,9 @@ class Command:
         # For more details see here:
         # https://learn.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=registry#enable-long-paths-in-windows-10-version-1607-and-later
         drive = self.deckFilePath.drive
+        root = self.deckFilePath.root
         deckFilePathString = str(self.deckFilePath)
-        pathLength = len(deckFilePathString) - len(drive)
+        pathLength = len(deckFilePathString) - len(drive) - len(root)
 
         if pathLength < self.MAX_DECK_FILE_PATH_LENGTH:
             return self.deckFilePath
