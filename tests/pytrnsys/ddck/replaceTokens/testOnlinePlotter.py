4683de68d459daa91edb8e42391c6df993695ecb
Damian Birchler <damian.birchler@ost.ch>
2025-09-17 10:22:18 +0200

    Add test for no spaces around bar.

--- tests/pytrnsys/ddck/replaceTokens/testOnlinePlotter.py
+++ tests/pytrnsys/ddck/replaceTokens/testOnlinePlotter.py
@@ -1027,6 +1027,62 @@ LABELS #
 """,
     )
 
+    yield TestCase(
+        "no-spaces",
+        """\
+***********************************
+** Online Plotter
+***********************************
+UNIT 103 TYPE 65		!Changed automatically
+PARAMETERS #     
+#     				! 1: Nb. of left-axis variables
+#     				! 2: Nb. of right-axis variables
+-0.5   				! 3: Left axis minimum
+100        			! 4: Left axis maximum
+-1   				! 5: Right axis minimum
+1e5       			! 6: Right axis maximum
+$nPlotsPerSim		! 7: Number of plots per simulation
+12     				! 8: X-axis gridpoints
+1     				! 9: Shut off Online w/o removing
+-1     				! 10: Logical unit for output file
+0     				! 11: Output file units
+0     				! 12: Output file delimiter
+INPUTS #    
+TSources TStore TNetReturn TMixed mixOn storeOn needStoreButTempTooLow bypassOff MSources MFromStore MMixed|
+TSources TStore TNetReturn TMixed mixOn storeOn needStoreButTempTooLow bypassOff MSources MFromStore MMixed|
+LABELS #     
+"Tempertures [degC] and statuses [1]"
+"Mass flow rates [kg/h]"
+ValByPass
+""",
+        """\
+***********************************
+** Online Plotter
+***********************************
+UNIT 103 TYPE 65		!Changed automatically
+PARAMETERS #     
+11     				! 1: Nb. of left-axis variables
+0     				! 2: Nb. of right-axis variables
+-0.5   				! 3: Left axis minimum
+100        			! 4: Left axis maximum
+-1   				! 5: Right axis minimum
+1e5       			! 6: Right axis maximum
+$nPlotsPerSim		! 7: Number of plots per simulation
+12     				! 8: X-axis gridpoints
+1     				! 9: Shut off Online w/o removing
+-1     				! 10: Logical unit for output file
+0     				! 11: Output file units
+0     				! 12: Output file delimiter
+INPUTS #    
+TSources TStore TNetReturn TMixed mixOn storeOn needStoreButTempTooLow bypassOff MSources MFromStore MMixed
+TSources TStore TNetReturn TMixed mixOn storeOn needStoreButTempTooLow bypassOff MSources MFromStore MMixed
+LABELS #     
+"Tempertures [degC] and statuses [1]"
+"Mass flow rates [kg/h]"
+ValByPass
+""",
+    )
+
 
 class TestOnlinePlotter:
     @_pt.mark.parametrize("testCase", [_pt.param(tc, id=tc.id) for tc in getTestCases()])
