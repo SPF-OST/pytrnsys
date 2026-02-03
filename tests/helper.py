292482ea93a4c86e08410d18b7502026237b6401
ahobeost <ahobeost@github.com>
2026-02-03 16:09:56 +0100

    Copy pasting error

--- tests/helper.py
+++ tests/helper.py
@@ -10,7 +10,6 @@ def is_run_during_ci() -> bool:
 
 def getTrnExeFilePath() -> _pl.Path:
     if is_run_during_ci():
-        return _pl.Path(r"C:\CI-Programs\TRNSYS18_pytrnsys\Exe")
-
+        return _pl.Path(r"C:\CI-Programs\TRNSYS18_pytrnsys\Exe\TrnEXE.exe")
 
     return _pl.Path(r"C:\TRNSYS18\Exe\TrnEXE.exe")
