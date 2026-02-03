6f555cabbb456abab64c0e47412d30f313c86148
ahobeost <ahobeost@github.com>
2026-02-03 15:56:10 +0100

    CI-progams - CI-programs

--- tests/helper.py
+++ tests/helper.py
@@ -3,11 +3,14 @@
 import os as _os
 import pathlib as _pl
 
+
 def is_run_during_ci() -> bool:
     return _os.environ.get("CI") == "true"
 
+
 def getTrnExeFilePath() -> _pl.Path:
     if is_run_during_ci():
-        return _pl.Path("C:/CI-Progams/TRNSYS18_pytrnsys/Exe/TrnEXE.exe")
+        return _pl.Path(r"C:\CI-Programs\TRNSYS18_pytrnsys\Exe")
 
-    return _pl.Path("C:/TRNSYS18/Exe/TrnEXE.exe")
+
+    return _pl.Path(r"C:\TRNSYS18\Exe\TrnEXE.exe")
